136a6cea4e3df18ef7f970978615b4d672c2819a
PPMMrMa <1302664733@qq.com>
2022-04-27 16:24:35 +0800

    4-27马

--- src/main/java/tool/Time.java
+++ src/main/java/tool/Time.java
@@ -1,17 +1,23 @@
 package tool;
 
+//时间类： 格式为12位的字符串，如2022年4月30日14点30 的对应字符串为：“202204301400”
 public class Time {
     private int year;
     private  int month;
     private  int day;
     private  int hour;
     private int minute;
-
+   //检查时间格式
     public static boolean CheckTime(String time){
         Time t=StringToTime(time);
-
-        return true;
+        if(time.length()!=12) {
+            return false;
+        }
+        else {
+          return juge(t.year,t.month,t.day);
+        }
     }
+    //将时间字符串转为时间类
     public  static Time StringToTime(String time){
       Time t=new Time();
       t.setYear((time.indexOf(0)-'0')*1000+(time.indexOf(1)-'0')*100+(time.indexOf(2)-'0')*10+(time.indexOf(3)-'0'));
@@ -62,5 +68,23 @@ public class Time {
     public void setMinute(int minute) {
         this.minute = minute;
     }
+    private static boolean juge(int year,int month,int day) {
+        //定义一个数组，数组里是每个月的合法天数
+        int[] arr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+        //判断平年闰年
+        if ( ( year % 4 == 0 && year % 100 != 0 ) || ( year % 400 == 0 ) ) {
+            arr[1] = 29;//闰年二月有29天
+        } else {
+            arr[1] = 28;//平年二月有28天
+        }
+
+        //首先月份只有1到12月
+        if (month > 0 && month < 13) {
+            if ( day <= arr[month - 1] && day > 0 ) {  //天数从0到该月的最大天数
+                return true;  //如果合法，返回true
+            }
+        }
+        return false;
+    }
 }
